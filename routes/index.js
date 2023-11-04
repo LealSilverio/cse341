6ee33b6ef54703575ac3f6adc23f559e6160bd2c
Sara Silverio <saralealsilverio@gmail.com>
2023-11-04 14:47:37 -0600

    project step 5 update

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => { res.send('Hello World')});
+router.get('/users', require('./users'));
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {console.log(`Listening on port ${port}`)});
+mongodb.connectToDb((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port);
+      console.log(`Web Server is listening at port ${port}`);
+      db = mongodb.getDb();
+    }
+});
