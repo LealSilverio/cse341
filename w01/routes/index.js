68ec9c8911d4689e47c7280504a1d90f295e8f65
Sara Silverio <saralealsilverio@gmail.com>
2023-11-04 13:10:27 -0600

    folder organization

--- w01/routes/index.js
+++
@@ -1,7 +0,0 @@
-const routes = require('express').Router();
-const lesson1Controller = require('../controllers/lesson1')
-
-routes.get('/', lesson1Controller.buildIndex)
-routes.get('/name', lesson1Controller.buildMyNamePage)
-
-module.exports = routes;
