989bdc9eaae53625a313d8c306f9197381cd0d75
Sara Silverio <saralealsilverio@gmail.com>
2023-11-11 20:32:02 -0700

    project part 2 update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
+const userController = require('../controllers/users')
 
 router.get('/', (req, res) => { res.send('Hello World')});
-router.get('/users', require('./users'));
+router.get('/users', userController.getAll);
 
 module.exports = router
