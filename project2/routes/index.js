1aaebcddc4506f765d2984163143cf8188fd6e82
Sara Silverio <saralealsilverio@gmail.com>
2023-11-25 20:27:01 -0700

    swagger update

--- project2/routes/index.js
+++ project2/routes/index.js
@@ -5,9 +5,12 @@ const repController = require('../controllers/recipes')
 router.get('/', (req, res) => { res.send(
     `<h1>Welcome to my CSE341 Food 101 repo</h1>
     <button><a href="/recipes">Recipes</a></button>
-    <button><a href="/desserts">Desserts</a></button>`
+    <button><a href="/desserts">Desserts</a></button>
+    <button><a href="/api-docs">API Documentation</a></button>`
 )});
+
 router.get('/recipes', repController.getAll);
-router.get('/desserts', repController.getAll)
+router.get('/desserts', repController.getAll);
+router.get('/api-docs', require('./swagger'));
 
 module.exports = router
--- project2/server.js
+++ project2/server.js
@@ -3,12 +3,14 @@ const app = express();
 const port = process.env.PORT || 8000;
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 // Routes
 app.use('/', require('./routes/index'));
